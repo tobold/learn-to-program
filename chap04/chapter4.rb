a1b5b3b029e8d97935186f79a1ed57bcf24fa226
Toby Hamand <tobyhamand@gmail.com>
2017-06-12 20:16:48 +0100

    Chapter 4

---
+++ chap04/chapter4.rb
@@ -0,0 +1,33 @@
+#Chapter 4
+puts '...you can say that again...'
+puts '...you can say that again...'
+
+my_string = '...you can say that again...'
+puts my_string
+puts my_string
+
+name = 'Toby Nicolas Hamand'
+puts 'My name is ' + name + '.'
+puts 'Wow! ' + name
+puts 'is awesome'
+
+composer = 'Mozart'
+puts composer + ' was "da bomb" in his day.'
+composer = 'Beethoven'
+puts 'But I prefer ' + composer + ', personally.'
+
+my_own_var = 'just another ' + 'string'
+puts my_own_var
+my_own_var = 5 * (1+2)
+puts my_own_var
+
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+
+puts ''
+
+var1 = 'eight'
+puts var1
+puts var2
