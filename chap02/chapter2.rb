9d536088e6b5a202a1aa7fd7c6db364281e7b827
Toby Hamand <tobyhamand@gmail.com>
2017-06-12 19:54:22 +0100

    Chapter 2

---
+++ chap02/chapter2.rb
@@ -0,0 +1,22 @@
+# Chapter 2
+puts 1 + 2
+
+#2.4
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+puts 8 * (15 - 3) + 19
+puts (8 + 3)
+puts (5.0 / 4.0)
+puts (8 + 3) - (5.0 / 4.0)
+
+#2.5
+puts "#{24 * 365} hours in a year"
+puts "#{60 * 24 * 365 * 10} minutes in a decade"
+puts "#{60 * 60 * 24 * 365 * 30.5} seconds in my life"
+puts "The author is #{1160000000.00 / 60 / 60 / 24 / 265} years old"
