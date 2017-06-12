65f169ba7e3c3a5b37310c4b4a6cabd9087a0be2
Toby Hamand <tobyhamand@gmail.com>
2017-06-12 20:08:18 +0100

    Chapter 3

---
+++ chap03/chapter3.rb
@@ -0,0 +1,26 @@
+#Chapter 3
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye'
+
+#3.1
+puts 'I like' + 'apple pie'
+puts 'I like ' + 'apple pie'
+puts 'I like' + ' apple pie'
+puts 'blink ' * 4
+
+#3.2
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+#3.3
+#puts '12' + 12
+#puts '2' * '5'
+puts 'You\'re swell'
+puts 'backslash at the end of a string:  \\'
+puts 'up\\down'
+puts 'up\down'
