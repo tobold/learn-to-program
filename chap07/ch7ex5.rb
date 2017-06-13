ead91a73474bbe3cb1543c852e11ade80863c9df
Toby Hamand <tobyhamand@gmail.com>
2017-06-13 20:02:41 +0100

    Chapter 7

--- chap07/ch7ex5.rb
+++ chap07/ch7ex5.rb
@@ -15,14 +15,35 @@ while true
 end
 
 puts "HELLO?"
-while true
+bye = 0
+while bye < 3
 	question = gets
 	year = 1930 + rand(21)
 	if question.chomp == 'BYE'
-		break
+		puts 'THAT\'S NICE DEAR'
+		bye = bye + 1
 	elsif question.chomp == question.upcase
 		puts 'NO, NOT SINCE ' + year.to_s
+		bye = 0
 	else
 		puts 'HUH?! SPEAK UP, SONNY!'
+		bye = 0
 	end
+end
+
+puts 'Enter starting year'
+year = gets
+year = year.to_i
+puts 'Enter ending year'
+endyear = gets
+endyear = endyear.to_i
+puts 'Leap years in the given year range are:'
+while year <= endyear
+	if year % 400 == 0
+		puts year
+	elsif year % 100 == 0
+	elsif year % 4 == 0
+		puts year
+	end
+	year = year + 1
 end
