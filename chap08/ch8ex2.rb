b1376fd4a8295a1d4e7e6191c5de9393b02357aa
Toby Hamand <tobyhamand@gmail.com>
2017-06-13 22:59:40 +0100

    Chapter 8 finished

---
+++ chap08/ch8ex2.rb
@@ -0,0 +1,26 @@
+#Chapter 8
+
+#8.2
+foods = ['artichoke', 'brioche', 'caramel']
+ 
+puts foods
+puts
+puts foods.to_s
+puts
+puts foods.join(', ')
+puts
+puts foods.join('  :)  ') + '  8)'
+
+puts [1, foods, 3]
+
+favorites = []
+favorites.push 'raindrops on roses'
+favorites.push 'whiskey on kittens'
+
+puts favorites[0]
+puts favorites.last
+puts favorites.length
+
+puts favorites.pop
+puts favorites
+puts favorites.length
---
+++ chap08/ch8ex3.rb
@@ -0,0 +1,27 @@
+#Chapter 8
+
+#8.3
+puts 'Enter words to be sorted'
+wordlist = []
+while true
+	word = gets.chomp
+	if word == ''
+		break
+	else
+		wordlist.push word
+	end
+end
+puts wordlist.sort
+
+tableofcontents = [	[Chapter 1:  Getting Started, 	1]
+					[Chapter 2:  Numbers, 			9]
+					[Chapter 3:  Letters, 			13] ]
+
+puts 'Table of Contents'.center(50)
+
+tableofcontents.each do |chapter|
+	chaptitle = chapter[0]
+	page = chapter[1]
+	puts chaptitle.ljust(30)
+	puts page.rjust(30)
+end
